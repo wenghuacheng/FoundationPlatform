ce20736ff6940c08510ef71c1bbdf2d4d43aa2da
wenghuacheng <593103283@qq.com>
2018-03-09 21:53:55 +0800

    web项目提交

---
+++ Repository.Dapper/DapperRepositoryBase.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq.Expressions;
+using System.Text;
+using Dapper;
+
+namespace Repository.Dapper
+{
+    public class DapperRepositoryBase<TEntity, TPrimaryKey> : RepositoryBase<TEntity, TPrimaryKey> where TEntity : class, IEntity<TPrimaryKey>
+    {
+        #region Connection
+        public virtual DbConnection Connection
+        {
+            get { return null; }
+        }
+        #endregion
+
+        public override int Count(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Execute(string query, object parameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TEntity FirstOrDefault(TPrimaryKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TEntity Get(TPrimaryKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TEntity> GetAllPaged(Expression<Func<TEntity, bool>> predicate, int pageNumber, int itemsPerPage, string sortingProperty, bool ascending = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TEntity> GetAllPaged(Expression<Func<TEntity, bool>> predicate, int pageNumber, int itemsPerPage, bool ascending = true, params Expression<Func<TEntity, object>>[] sortingExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TPrimaryKey InsertAndGetId(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TEntity> Query(string query, object parameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<TAny> Query<TAny>(string query, object parameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TEntity Single(TPrimaryKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TEntity Single(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository.Dapper/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Dapper
+{
+    public interface IEntity<T> : IEntity
+    {
+        T Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+
+    }
+}
